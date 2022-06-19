7cd719ed977d73f81d817659f1c5c859517caf02
souparno24 <sou24par08no1999@gmail.com>
2022-06-19 21:49:29 +0530

    20th june

--- app/models/image.rb
+++ app/models/image.rb
@@ -10,6 +10,9 @@ class Image < ApplicationRecord
         if image.attached? == false
             errors.add(:image,"is missing!!!!!")
         end
+        if !image.content_type.in?(%('image/jpeg image/png'))
+            errors.add(:image,"needs to be a jpeg or png")
+        end
     end
    
 end
